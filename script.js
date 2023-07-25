fc519cd3d70729f9aa589fa8f26501a6e8a66f47
Cyril Derc <cyril_derc@hotmail.com>
2023-07-26 00:06:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Créer un événement au scroll
+window.addEventListener("scroll", () => {
+  //   Navbar effect
+
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "90px";
+  }
+
+  // Faire apparaitre l'image
+});
+// Réduire la navbar quand on descend vers le bas, la remettre à sa taille initiale si on remonte tout en haut
+
+// Faire apparaitre l'image de la partie improvise
+
+// Faire apparaitre la popup quand on est en bas du site
+
+// Bonus : quand on clicke sur la popup elle disparait pour toujours
